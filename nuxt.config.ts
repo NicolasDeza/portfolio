99522dad08b656a0af8a86af52b2e83ed5897ca2
NicolasDeza <nicolasdeza@hotmail.be>
2025-12-21 12:32:49 +0100

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -49,8 +49,8 @@ export default defineNuxtConfig({
       link: [
         {
           rel: "icon",
-          type: "image/png",
-          href: "/favicon.png",
+          type: "image/x-icon",
+          href: "/favicon.ico",
         },
       ],
     },
