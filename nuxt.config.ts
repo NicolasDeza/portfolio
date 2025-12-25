beb808963da2329a770e5464a3fbeb0fdb85ea26
NicolasDeza <nicolasdeza@hotmail.be>
2025-12-25 17:22:13 +0100

    apple touch

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -83,9 +83,9 @@ export default defineNuxtConfig({
           href: "/favicon.ico",
         },
         {
-          // rel: "apple-touch-icon",
-          // sizes: "180x180",
-          // href: "/nuxt-logo.png", // Pour iOS
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
         },
       ],
     },
